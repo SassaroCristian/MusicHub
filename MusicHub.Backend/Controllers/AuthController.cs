4400ec4a20546a3c1ac2b57a0b7f0028d34ddc22
SassaroCristian <sassaro01cristian@gmail.com>
2025-01-28 13:40:58 +0100

    Implement Spotify login and callback flow

---
+++ MusicHub.Backend/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MusicHub.Backend.Controllers
+{
+	public class AuthController : Controller
+	{
+		[HttpGet("login")]
+		public IActionResult Login()
+		{
+			return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "Spotify");
+		}
+
+		[HttpGet("callback")]
+		public async Task<IActionResult> Callback()
+		{
+			var authenticateResult = await HttpContext.AuthenticateAsync("Spotify");
+			if (!authenticateResult.Succeeded)
+			{
+                return Unauthorized();
+            }
+			var accessToken = authenticateResult.Properties.GetTokenValue("access_token");
+            HttpContext.Session.SetString("AccessToken", accessToken);
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
+
